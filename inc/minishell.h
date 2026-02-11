967c0c8ef9088a072d77aa55b83cce075a190a06
Mathis Tagand <mtagand@f6r4s11.clusters.42paris.fr>
2026-02-11 11:32:46 +0100

    structure

---
+++ inc/minishell.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtagand <mtagand@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 11:09:00 by mtagand           #+#    #+#             */
+/*   Updated: 2026/02/11 11:21:43 by mtagand          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef enum e_type {
+    WORD,         // Pour ls, -l, "mon fichier"
+    PIPE,         // |
+    REDIR_IN,     // <
+    REDIR_OUT,    // >
+    D_REDIR_IN,   // <<
+    D_REDIR_OUT   // >>
+} t_type;
+
+typedef struct s_token {
+    char            *content;
+    t_type          type;
+    struct s_token  *next;
+    struct s_token  *prev;
+} t_token;
+
+typedef struct s_token_list {
+    t_token         *head;
+    t_token         *tail;
+    int             size;
+} t_token_list;
+
+typedef struct  s_env_copy {
+    char *line;
+    char *var;
+    char *content;
+    struct s_env_copy *next;
+} t_env_copy;
