58f5f52fce82f5770a90c42ea2f86979c39a786b
Cyril-glitch <cyrilcolonneau@hotmail.fr>
2026-02-19 17:15:42 +0100

    init_src update depuis expand

---
+++ init_src/shell_exit.c
@@ -0,0 +1,36 @@
+#include "../inc/minishell.h"
+
+void    ft_free_data(t_data *data)
+{
+    if (!data)
+        return ;
+    if (data->line)
+        free(data->line);
+    if (data->env_list)
+        ft_lstclear_env(&data->env_list);
+    if (data->token_list)
+        ft_db_lstclear(data->token_list, free);
+    if (data->sig_a)
+        free(data->sig_a);
+    if (data->orig_termios)
+        free(data->orig_termios);
+    if (data->new_termios)
+        free(data->new_termios);
+    if (data->expd)
+    {
+        ft_expd_zero(data->expd);
+        free(data->expd);
+    }
+    if (data->expd_lst)
+        ft_lstclear(&data->expd_lst, free);
+    free(data);
+    data = NULL;
+}
+
+void	ft_shell_exit(t_data *data)
+{
+	tcsetattr(0, TCSANOW, data->orig_termios);
+    ft_free_data(data);
+	rl_clear_history();
+	exit(0);
+}
