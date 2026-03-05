4afa61057fd21a8a110a35904df6e8cedbc4ae4d
cyril <cyrilcolonneau@hotmail.fr>
2026-03-05 14:24:28 +0100

    fini 1er test ok

--- expand_src/split_expand.c
+++ expand_src/split_expand.c
@@ -1,38 +1,51 @@
 #include "../inc/minishell.h"
 
-int	ft_q_status(char quote, char c)
+static t_token_list *ft_add_split_token_lst(void)
 {
-	if (quote && (c == '\'' || c == '\"'))
+    t_token_list *new;
+
+    new = malloc(sizeof(t_token_list));
+    if (!new)
+        return (NULL);
+    new->head = NULL;
+    new->tail = NULL;
+    new->size = 0;
+    return new;
+}
+
+static int	ft_q_status(char quote, char c)
+{
+	if (!quote && (c == '\'' || c == '\"'))
 		return (c);
 	else if (quote && c == quote)
 		return (0);
-	return (0);
+	return (quote);
 }
 
-static void ft_splitoken(char *str, t_token_list *newlst, t_data *data, char quote)
+static void ft_splitoken(t_token *cur, char *str, t_token_list *newlst, t_data *data, char quote)
 {
-    int i;
     t_token *new;
     char *tmp;
+    int i;
 
     i = 0;
     new = ft_db_lstnew_token();
     if (!new)
         ft_shell_exit(data);
-    while (str[i] && str[i] != ' ')
+    while ((str[i] && str[i] != ' ') || quote == '\"')
         quote = ft_q_status(quote ,str[i++]);
-    if (quote != '\"')
-    {
-        tmp = ft_substr(str,0,i);
-        new->content = ft_delquote(tmp);
-        free(tmp);
-        ft_db_lstadd_front_token(newlst, new);
-    }
-    if (str[i])
-    {
-        str += i;
-        ft_splitoken(str, newlst, data, quote);
-    }
+    tmp = ft_substr(str,0,i);
+    if (!tmp)
+        ft_shell_exit(data);
+    new->content = ft_delquote(tmp);
+    new->type = cur->type;
+    if (!new)
+        ft_shell_exit(data);
+    free(tmp);
+    ft_db_lstadd_back_token(newlst, new);
+    str += (i + (str[i] == ' '));
+    if (*str)
+        ft_splitoken(cur, str , newlst, data, quote);
 }
 
 void ft_split_expand(t_token_list *lst, t_data *data)
@@ -41,12 +54,15 @@ void ft_split_expand(t_token_list *lst, t_data *data)
     t_token_list *newlst;
 
     current = lst->head;
-    newlst = NULL;
+    newlst = ft_add_split_token_lst();
+    if (!newlst)
+        ft_shell_exit(data);
     while (current)
     {
-        ft_splitoken(current->content, newlst, data, 0);
+        ft_splitoken(current, current->content, newlst, data, 0);
         current = current->next;
     }
-	ft_db_lstclear_token(lst , free);
-    lst = newlst;
+	ft_db_lstclear_token(data->token_list , free);
+    free(data->token_list);
+    data->token_list = newlst;
 }
