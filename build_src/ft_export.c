98ac3a021ce90ea7072d15fc12b8f03343a3982a
cyril <cyrilcolonneau@hotmail.fr>
2026-03-06 14:26:15 +0100

    solidification printexport

--- build_src/ft_export.c
+++ build_src/ft_export.c
@@ -71,6 +71,7 @@ static void	ft_printlst_export(t_env_list *env_list, t_data *data)
 {
 	char	**tab;
 	int		size;
+    char *dup;
 
 	size = 0;
 	tab = ft_tab_list(env_list);
@@ -83,9 +84,10 @@ static void	ft_printlst_export(t_env_list *env_list, t_data *data)
 	while (tab[size])
 	{
 		ft_putstr("export ");
-        ft_putstr(ft_key_hunter(tab[size], env_list, data)->key);
+        dup = ft_key_hunter(tab[size], env_list, data)->key;
 		ft_putstr("=\"");
-        ft_putstr(ft_key_hunter(tab[size], env_list, data)->content);
+        if (dup)
+            ft_putstr(dup);
 		ft_putstr("\"");
 		write(1, "\n", 1);
 		size++;
